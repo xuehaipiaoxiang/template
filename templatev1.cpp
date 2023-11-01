f9b0fe4aa6d3c3a3c6b802917212a85f54a0f3db
xuehaipiaoxiang <56183476+xuehaipiaoxiang@users.noreply.github.com>
2023-11-01 16:31:40 +0800

    Update templatev1.cpp

--- templatev1.cpp
+++ templatev1.cpp
@@ -1,1 +1,35 @@
+//*****************************************************trie****************************
+/*
+字典树
+*/
+const int N = 10010;
 
+char table1[N][26];
+int idx1; // index 始终增加
+bool visited[N]; // 可以改成计数
+
+void insert( char  str1[])
+{
+    int cur = 0;
+    for(int i = 0; str1[i] != '\0'; ++i)
+    {
+        int tmp = str1[i] - 'a';
+        if(table1[cur][tmp] == 0) 
+            table1[cur][tmp] = ++idx1;
+        cur = table1[cur][tmp]
+    }
+    visited[cur] = true;
+}
+
+bool find ( char str1[])
+{
+    int cur = 0;
+    for(int i = 0; str1[i] != '\0'; ++i)
+    {
+        int tmp = str1[i] - 'a';
+        if(table1[cur][tmp] == 0) 
+            return false
+        cur = table1[cur][tmp]
+    }
+    return visited[cur];
+}
